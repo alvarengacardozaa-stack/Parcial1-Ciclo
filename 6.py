ccbd6b07f22ada71524475ee46ac2b08a36d55ef
alvarengacardozaa <alvarengacardozaa@gmail.com>
2026-04-11 16:32:44 -0300

    Parcial 02 Fundamentos de programación, Ejercicio 6, normalización casefold y validación alfabética

---
+++ 6.py
@@ -0,0 +1,8 @@
+# EJERCICIO 6: casefold() e isalpha()
+
+texto6 = "Patrick"
+
+normalizado6 = texto6.casefold()
+es_alfa6 = normalizado6.isalpha()
+
+print(f"E6 {normalizado6}, ¿es alfabetico?: {es_alfa6}")
