2aad6acf6d2c87c2fdfd1ffcd8ae4d28f98ccde5
alvarengacardozaa <alvarengacardozaa@gmail.com>
2026-04-11 16:15:42 -0300

    Parcial 02 Fundamentos de programación, Ejercicio 2, formato título y reemplazo de nombre por apellido

---
+++ 2.py
@@ -0,0 +1,12 @@
+# EJERCICIO 2: title() y replace()
+# 1. Cadena original, 2. Formato Título, 3. Reemplazo
+texto = "patrick cardoza"
+print(texto)
+
+## Convertir la primera letra de cada palabra en mayúscula
+texto_titulo = texto.title()
+print(texto_titulo)
+
+## Reemplazar "Su Nombre" por "Su Apellido"
+texto_reemplazado = texto_titulo.replace("Patrick", "Cardoza")
+print(texto_reemplazado)
