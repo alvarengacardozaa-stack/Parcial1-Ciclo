0479becb7cacf0a8da5dfe18af176a46b412ffc6
alvarengacardozaa <alvarengacardozaa@gmail.com>
2026-04-11 16:10:12 -0300

    Parcial 02 Fundamentos de programación, Ejercicio 1, limpieza de espacios con conteo de letra e

---
+++ 1.py
@@ -0,0 +1,9 @@
+# EJERCICIO 1: strip() y count()
+# 1. Declarar variable, 2. Limpiar espacios, 3. Contar las 'e'
+animal = " elefante "
+
+limpio = animal.strip()
+
+conteo = limpio.count("e")
+
+print(f"E1: {limpio}, Conteo de las 'e': {conteo}")
