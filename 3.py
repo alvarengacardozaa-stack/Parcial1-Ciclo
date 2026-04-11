84f1fc32f291baa3d1cf1728548fd33135f04192
alvarengacardozaa <alvarengacardozaa@gmail.com>
2026-04-11 16:23:28 -0300

    Parcial 02 Fundamentos de programación, Ejercicio 3, remoción de prefijo ING y conversión a mayúsculas

---
+++ 3.py
@@ -0,0 +1,8 @@
+# EJERCICIO 3: removeprefix() y upper()
+texto = "ING. Patrick Alejandro Alvarenga Cardoza"
+
+sin_prefijo = texto.removeprefix("ING. ")
+
+mayusculas = sin_prefijo.upper()
+
+print(mayusculas)
